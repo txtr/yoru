6f926c4627443977ca9978506de3a7d174a847db
txtr <32964043+txtr@users.noreply.github.com>
2020-07-28 17:39:54 +0530

    name

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 """This module contains a template MindMeld application"""
 from yoru.root import app
-import yoru.greeting
+import yoru.greeting_handler
 __all__ = ['app']
 
 
 @app.handle(default=True)
 def default(request, responder):
     """This is a default handler."""
-    responder.reply('Hello there!')
+    responder.reply('DEFAULT HANDLER REACHED.')
