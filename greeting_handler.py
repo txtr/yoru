f06c123a817915c390dfa725c3e58918c437095e
Shubham Pednekar <32964043+txtr@users.noreply.github.com>
2020-07-28 17:10:02 +0530

    Update greeting_handler.py

--- greeting_handler.py
+++ greeting_handler.py
@@ -8,22 +8,19 @@ from .root import app
 def welcome(request, responder):
     try:
         name = responder.slots['name']
-        response = 'Hello, '+ name +'. Its me Yoru. How can I help you?'
+        response = 'Hello, { name }. Its me Yoru. How can I help you?'
     except KeyError:
         responder.reply('Hello. Welcome aboard Oceanic Symphony, I am your assistant YORU. What can i call you?')
-        responder.listen()
         responder.params.target_dialogue_state = 'get_user_name'
     responder.listen()
 
 @app.handle(targeted_only=True)
 def get_user_name(request, responder):
     try:
-        name = response.context['name']
+        responder.slots['name'] = response.context['name']
     except KeyError:
-        name = request.text
-    responder.slots['name'] = name
+        responder.slots['name'] = request.text
     responder.reply('Nice to meet you,  {name}. How can I help you?')
-
 
 
 @app.handle(intent='exit')
@@ -32,4 +29,5 @@ def say_bye(request, responder):
         name = responder.slots['name']
     except:
         name = ''
-    responder.reply('Good Bye.', 'See ya.', 'Bye '+ name +', have a good day.', 'Bye bye!', 'See you soon.', 'Untill next time!', 'It was nice seeing you')
+    responder.reply('Bye '+ name +', have a good day.')
+    # responder.reply('Good Bye.', 'See ya.', 'Bye '+ name +', have a good day.', 'Bye bye!', 'See you soon.', 'Untill next time!', 'It was nice seeing you')
