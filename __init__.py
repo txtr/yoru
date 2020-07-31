33b4c2dcd0fa7f8d5e5e8c35ef5563469770f4ba
Shaikh Khalid <shaikhalidwork@gmail.com>
2020-08-01 00:50:27 +0530

    added events

--- __init__.py
+++ __init__.py
@@ -3,10 +3,12 @@
 from yoru.root import app
 import yoru.greeting_handler
 import yoru.food_ordering_handler
+import yoru.events_handler
+# import yoru.day_planner_handler
 __all__ = ['app']
 
 
-# @app.handle(default=True)
-# def default(request, responder):
-#     """This is a default handler."""
-#     responder.reply('GLOBAL DEFAULT HANDLER REACHED.')
+@app.handle(default=True)
+def default(request, responder):
+    """This is a default handler."""
+    responder.reply('GLOBAL DEFAULT HANDLER REACHED.')
